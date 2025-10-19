72e665bd2d5c2d35bcf26f4108bcdc2a3ab7d06a
Ender Rencüzoğulları <34414342+earcz@users.noreply.github.com>
2025-10-20 00:29:05 +0300

    Update theme.py

--- core/theme.py
+++ core/theme.py
@@ -95,7 +95,7 @@ def apply_minimal_theme():
 
     .carioca-header .user-img {
         position: absolute;
-        top: 1.2rem;
+        top: 1.5rem;
         right: 1.4rem;
         width: 120px;
         height: 120px;
